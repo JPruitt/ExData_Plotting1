ae31dc788cf63615c1b828c10a16ea176238fa7d
Joe Pruitt <rgrjoey001@hotmail.com>
2014-05-07 15:42:03 -0500

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,35 @@
+## Read in data
+d<-read.table("household_power_consumption.txt", 
+              sep=";", 
+              skip = 1,
+              col.names=c("Date", "Time", "Global_active_power", "Global_reactive_power",
+                          "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2",
+                          "Sub_metering_3"),
+              colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", 
+                           "numeric", "numeric", "numeric"),
+              na.strings = "?",
+              fill=F,
+              strip.white=T)
+
+## Convert Date column to date class
+d$Date<-as.Date(d$Date, "%d/%m/%Y")
+
+## Subset the data to the two days of interest
+subD<-subset(d, d$Date == "2007-02-01" | d$Date == "2007-02-02")
+
+## Create date-time column and convert to date-time class
+subD$Date<-as.character(subD$Date)
+subD["dateTime"]<-paste(subD$Date, subD$Time)
+subD$dateTime<-strptime(subD$dateTime, "%Y-%m-%d %H:%M:%S")
+str(subD$dateTime)
+
+## Open png device
+png(file = "plot2.png", width = 480, height = 480, units = "px") 
+
+## Create plot and send to png file 
+## Plot 2
+plot(subD$dateTime, subD$Global_active_power, type="l", 
+     ylab="Global Active Power (kilowatts)", xlab="")
+
+## Close the png device
+dev.off() 
